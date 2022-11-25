dfd96ca586afd1c90facae39332904a42f88756d
itsevgenii <smazanovich@icloud.com>
2022-11-25 06:19:58 +0000

    committing modified changes

--- organiser/src/components/App/App.js
+++ organiser/src/components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import List from "../List";
 import Input from "../Input";
@@ -11,21 +11,29 @@ function App() {
     setTodos([...toDos, text]);
   }
 
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("text", JSON.stringify(toDos));
+  }, [toDos]);
+
   function handleDelete(i) {
     setTodos([...toDos.slice(0, i), ...toDos.slice(i + 1)]);
   }
 
   return (
-    <div className="App">
-      <div className="toDoContainer">
-        <h1>Today, you aim at achieving the following: </h1>
-        <div className="inputButton">
-          <Input onData={addTodo} />
-          <List toDos={toDos} handleDelete={handleDelete} />
+    <div>
+      <h1 className="LuckYHeading">Lucky Number 7 take on an organiser</h1>
+      <div className="App">
+        <div className="toDoContainer">
+          <h1>Today, you aim at achieving the following: </h1>
+          <div className="inputButton">
+            <Input onData={addTodo} />
+            <List toDos={toDos} handleDelete={handleDelete} />
+          </div>
         </div>
-      </div>
-      <div className="TechDisplay">
-        <TechDisplay />
+        <div className="TechDisplay">
+          <TechDisplay />
+        </div>
       </div>
     </div>
   );
